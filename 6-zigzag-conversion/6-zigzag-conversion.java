043aaf099dda960c687da75f7aee103ff4533c23
Hariharasudhan369 <67356790+Hariharasudhan369@users.noreply.github.com>
2022-07-31 19:49:22 +0530

    Time: 36 ms (30.33%), Space: 67.5 MB (9.26%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.java
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -2,28 +2,44 @@ class Solution {
     public String convert(String s, int numRows) {
         
         List<List<Character>> list = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
+        
+        for (int i = 0; i < numRows; ++i) {
+            
             List<Character> temp = new ArrayList<>();
+            
             list.add(temp);
+            
         }
 		
         int index = 0;
+        
         while (index < s.length()) {
             
-            for (int i = 0; i < numRows; i++) {
-                if (index >= s.length()) break;;
-                list.get(i).add(s.charAt(index++));
+            for (int i = 0; i < numRows; ++i) {
+                
+                if (index < s.length()){
+                    
+                    list.get(i).add(s.charAt(index++));
+                    
+                }
             }
             
-            for (int i = numRows - 2; i > 0 ; i--) {
-                if (index >= s.length()) break;;
-                list.get(i).add(s.charAt(index++));
+            for (int i = numRows - 2; i > 0 ; --i) {
+                
+                if (index < s.length()){
+                    
+                    list.get(i).add(s.charAt(index++));
+                    
+                } 
+                
             }
         }
 		
         String str = "";
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 0; j < list.get(i).size(); j++) {
+        for (int i = 0; i < list.size(); ++i) {
+            
+            for (int j = 0; j < list.get(i).size(); ++j) {
+                
                 str = str + list.get(i).get(j);
             }
         }
